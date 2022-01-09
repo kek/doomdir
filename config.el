2753c738c1565b0b0395e66fe674ec89eb43c6d0
Karl Eklund <localpart@gmail.com>
2022-01-09 03:49:42 +0100

    use proportional fonts for some modes

--- config.el
+++ config.el
@@ -49,7 +49,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "Hack NF" :size 18))
+(setq doom-font (font-spec :family "Hack NF" :size 18)
+      doom-variable-pitch-font (font-spec :family "Ebrima" :size 20)
+      doom-big-font (font-spec :family "Hack NF" :size 24))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -114,3 +116,7 @@
 
 (display-battery-mode)
 
+(add-hook 'org-mode-hook #'variable-pitch-mode)
+(add-hook 'help-mode-hook #'variable-pitch-mode)
+(add-hook 'Info-mode-hook #'variable-pitch-mode)
+(add-hook 'emacs-startup-hook #'toggle-frame-fullscreen)
