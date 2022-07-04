745105e13f51f59687378c8855bd8ff637547ba9
Karl Eklund <localpart@gmail.com>
2022-07-04 14:26:34 +0200

    keybindings

--- config.el
+++ config.el
@@ -146,6 +146,7 @@
 
          (map! :after company :map company-active-map "<escape>" #'company-abort)
          (map! :map doom-leader-map "§" #'evil-switch-to-windows-last-buffer)
+         (map! :map doom-leader-map "d" #'duplicate-line)
          (map! :map elpher-mode-map "DEL" #'transient-noop)
          (map! :n "§" #'evil-execute-in-emacs-state)
          (map! :n "C-<left>" #'previous-buffer)
@@ -243,7 +244,7 @@
 (setq confirm-kill-emacs nil)
 (menu-bar-mode)
 
-(global-set-key (kbd "<f1>") #'info)
+(global-set-key (kbd "<f1>") (lambda () "open config.el" (interactive) (find-file (concat doom-private-dir "config.el"))))
 (global-set-key (kbd "<f2>") #'save-buffer)
 (global-set-key (kbd "<f3>") #'find-file)
 (global-set-key (kbd "<f4>") nil)
