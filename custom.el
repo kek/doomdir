ca97633b57abde929be1b46ca6747684be614d69
Karl Eklund <localpart@gmail.com>
2022-11-22 13:52:57 +0100

    safe-local-variable-values

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((diff-add-log-use-relative-names . t)
+     (vc-git-annotate-switches . "-w")))
  '(warning-suppress-types '((emacs) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
