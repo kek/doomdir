c5b10f710fddada22cb2337a81c50e365a2c2f30
Karl Eklund <localpart@gmail.com>
2023-01-03 23:25:57 +0100

    actually set line spacing?

--- config.el
+++ config.el
@@ -99,7 +99,6 @@
                 (equal emacs-version "29.0.60"))
            (progn
              (setq-default line-spacing 4)
-             (setq line-spacing nil)
              (pixel-scroll-precision-mode)
              (setq pixel-scroll-precision-interpolate-page nil)
              ;; (define-key pixel-scroll-precision-mode-map (kbd "C-v") #'pixel-scroll-interpolate-down)
