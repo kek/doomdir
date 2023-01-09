d99d9fdbd210d8b65e39df3c5e95443674eb5658
Karl Eklund <localpart@gmail.com>
2023-01-09 12:39:00 +0100

    Increase font size

--- config.el
+++ config.el
@@ -63,7 +63,7 @@
       my-font-size-windows 22
       my-font-size-wsl 22
       my-font-size-linux 20
-      my-font-size-linux-4k 22
+      my-font-size-linux-4k 24
       my-preferred-font-size (cond (my-is-wsl my-font-size-wsl)
                                    (my-is-windows my-font-size-windows)
                                    (my-is-linux-4k my-font-size-linux-4k)
