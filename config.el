b828a48b05362cc8aaf15ca38030bd01eb5b2e78
Karl Eklund <localpart@gmail.com>
2023-01-24 18:56:40 +0100

    mappings

--- config.el
+++ config.el
@@ -63,7 +63,7 @@
       my-is-linux-4k (and my-is-linux (equal (downcase (system-name)) "potatis"))
       my-font-size-windows 22
       my-font-size-wsl 22
-      my-font-size-linux 20
+      my-font-size-linux 16
       my-font-size-linux-4k 24
       my-preferred-font-size (cond (my-is-wsl my-font-size-wsl)
                                    (my-is-windows my-font-size-windows)
@@ -224,6 +224,8 @@
     :config
     (company-posframe-mode 1))
 (after! org
+  (define-key org-mode-map (kbd "<f9>") #'org-refile)
+  (define-key org-mode-map (kbd "C-<f9>") #'+org/refile-to-last-location)
   (add-hook 'org-mode-hook #'my-face-adjustments)
   (add-hook 'org-mode-hook #'my-disable-hl-line-mode)
   ;; (add-hook 'org-mode-hook #'my-org-margins)
@@ -401,7 +403,7 @@
          ;; (map! :map doom-leader-map "§" #'evil-switch-to-windows-last-buffer)
          (map! :map doom-leader-map "d" #'duplicate-line)
          (map! :map elpher-mode-map "DEL" #'transient-noop)
-         (map! :n "<f9>" #'evil-execute-in-emacs-state)
+         ;; (map! :n "<f9>" #'evil-execute-in-emacs-state)
          (map! :n "å" #'evil-execute-in-emacs-state)
          (map! :n "ä" nil)
          (map! :n "C-<left>" #'previous-buffer)
